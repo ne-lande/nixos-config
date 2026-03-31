66250906896e831e0463e151b7e21a587dbb0b8a
ne-lande <reo197624@gmail.com>
2026-03-31 10:19:42 +0300

    nvidia 6.19 kernel

--- hosts/kasen/nvidia.nix
+++ hosts/kasen/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
@@ -10,23 +10,6 @@
     powerManagement.enable = false;
     open = true;
     nvidiaSettings = true;
-    # unless kernel module is supported on 6.19
-    # https://github.com/NixOS/nixpkgs/issues/489947
-    # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/1021
-    # package = config.boot.kernelPackages.nvidiaPackages.latest;
-    package =
-      let
-        base = config.boot.kernelPackages.nvidiaPackages.latest;
-        cachyos-nvidia-patch = pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-        };
-      in
-      base
-      // {
-        open = base.open.overrideAttrs (oldAttrs: {
-          patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
-        });
-      };
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
